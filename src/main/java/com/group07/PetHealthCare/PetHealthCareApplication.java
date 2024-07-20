feea5f7d3d121ab5ed625912e359bdcf230178d7
Mtri <tranminhtri10213@gmail.com>
2024-07-20 14:27:42 +0700

    First commit

---
+++ src/main/java/com/group07/PetHealthCare/PetHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.group07.PetHealthCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHealthCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group07/PetHealthCare/PetHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group07.PetHealthCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
