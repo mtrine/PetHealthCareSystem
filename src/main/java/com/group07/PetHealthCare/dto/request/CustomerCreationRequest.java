f9be7d289f0985ec2b1f40d5daf6c3afb9fafef7
Mtri <tranminhtri10213@gmail.com>
2024-07-22 12:31:22 +0700

    mvc structure

---
+++ src/main/java/com/group07/PetHealthCare/dto/request/CustomerCreationRequest.java
@@ -0,0 +1,60 @@
+package com.group07.PetHealthCare.dto.request;
+
+import jakarta.persistence.Column;
+
+public class CustomerCreationRequest {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String sex;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
