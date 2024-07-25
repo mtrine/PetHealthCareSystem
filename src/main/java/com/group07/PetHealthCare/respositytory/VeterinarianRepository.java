55386c92c2ec7ab836a6c9ad30d60d1b1e30df4b
Qnhu <tranminhtri10213@gmail.com>
2024-07-25 10:24:22 +0700

    delete Customer

--- src/main/java/com/group07/PetHealthCare/respositytory/VeterinarianRepository.java
+++ src/main/java/com/group07/PetHealthCare/respositytory/VeterinarianRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface VeterinarianRepository extends JpaRepository<Veterinarian, String> {
     Optional<Veterinarian> findByEmail(String email);
+    
 }
--- src/main/java/com/group07/PetHealthCare/service/VeterinarianService.java
+++ src/main/java/com/group07/PetHealthCare/service/VeterinarianService.java
@@ -1,13 +1,27 @@
 package com.group07.PetHealthCare.service;
 
+import com.group07.PetHealthCare.pojo.Veterinarian;
 import com.group07.PetHealthCare.respositytory.VeterinarianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VeterinarianService {
     @Autowired
     private VeterinarianRepository veterinarianRepository;
 
+    public List<Veterinarian> getAllVeterinarian() {return veterinarianRepository.findAll();}
+
+    public Optional<Veterinarian> getVeterinarianById (String Id)
+    {
+        return veterinarianRepository.findById(Id);
+    }
+    public void deleteVeterinarianById(String Id)
+    {
+        veterinarianRepository.deleteById(Id);
+    }
 
 }
